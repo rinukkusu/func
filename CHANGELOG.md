0d3e575c929519b907a99f377f4369cafad2631d
rinukkusu <rinukkusu@sub-r.de>
2018-11-21 13:15:03 +0100

    make pub analyzer happy

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 2.0.1
+
+- make pub analyzer happy
+
+## 2.0.0
+
+- upgrade to Dart SDK 2.0
---
+++ example/example.dart
@@ -0,0 +1,7 @@
+import 'package:func2/func.dart';
+
+void main() {
+  Func2 add = (a, b) => a + b;
+
+  print(add(1, 2));
+}
